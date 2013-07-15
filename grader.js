3ac2b6d4793b0704baf52332f1ec4dc86b252aad
jferrer <jordiferrerrosera@yahoo.com>
2013-07-15 23:38:40 +0200

    hw4 part 1

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,16 +37,13 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
+
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function($, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -61,14 +59,36 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
+var checkHtml= function(buffer, checksfile){
+    var checkJson = checkHtmlFile(buffer,checksfile);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
+
 if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <url_address>','Url to htmlfile')
+        .parse(process.argv);
+ if (!program.file && !program.url) {
+   checkHtml(cheerio.load(fs.readFileSync(assertFileExists(HTMLFILE_DEFAULT))),program.checks);
+ } else if (program.file && !program.url){
+   checkHtml(cheerio.load(fs.readFileSync(assertFileExists(program.file))),program.checks);
+    } else if (!program.file && program.url){
+         rest.get(program.url).on('complete', function(result){
+           if (result instanceof Error) {
+              console.error('Error: ' + util.format(response.message));
+           } else {
+              checkHtml(cheerio.load(result),program.checks);
+           }
+         });
+    } else {
+        console.log("option -f and -u can't be used simultaneously ");
+    };
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
--- web.js
+++ web.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var fs =require('fs');
+var htmlfile="index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFile('index.html',function(err,data){
+  fs.readFile(htmlfile,function(err,data){
       if (err) throw error;
       response.send(data.toString('utf-8'));
   });
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
+var port = process.env.PORT || 8080;
+  app.listen(port, function() {
   console.log("Listening on " + port);
 });
